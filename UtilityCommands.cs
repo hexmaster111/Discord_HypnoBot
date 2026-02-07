f46d33dd3ed5416cc45896d83e0bc5e1b200a2ec
Hailey <hailey@not_an_email.fake>
2026-02-06 19:43:23 -0600

    added utility commands

---
+++ UtilityCommands.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using NetCord;
+using NetCord.Services.ApplicationCommands;
+
+public class UtilityCommands :
+    ApplicationCommandModule<ApplicationCommandContext>
+{
+    [SlashCommand("f_to_c", "Convert Deg F to deg C",
+        Contexts =
+            [InteractionContextType.DMChannel, InteractionContextType.Guild, InteractionContextType.BotDMChannel])]
+    public async Task<string> FToC(float degF)
+    {
+        var degC = degF;
+        degC -= 32;
+        degC *= 5.0f / 9.0f;
+        return $"`{degF:0.00}°F -> {degC:0.00}°C`";
+    }
+
+    [SlashCommand("c_to_f", "Convert Deg C to deg F",
+        Contexts =
+            [InteractionContextType.DMChannel, InteractionContextType.Guild, InteractionContextType.BotDMChannel])]
+    public async Task<string> CToF(float degC)
+    {
+        var degF = degC;
+        degF *= 9.0f / 5.0f;
+        degF += 32;
+
+        return $"`{degC:0.00}°C -> {degF:0.00}°F`";
+    }
+    
+    
+    
+}
